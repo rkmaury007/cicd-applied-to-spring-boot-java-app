46bf08c63cbd73e3d1ebc353557115e988aa86c4
Rahul Kr Maurya <rkmaurya2710@gmail.com>
2019-12-14 16:23:24 +0530

    Update CicdAppliedToSpringBootJavaAppApplicationTests.java

--- src/test/java/com/cicd/cicdappliedtospringbootjavaapp/CicdAppliedToSpringBootJavaAppApplicationTests.java
+++ src/test/java/com/cicd/cicdappliedtospringbootjavaapp/CicdAppliedToSpringBootJavaAppApplicationTests.java
@@ -2,7 +2,7 @@ package com.cicd.cicdappliedtospringbootjavaapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//tester
 @SpringBootTest
 class CicdAppliedToSpringBootJavaAppApplicationTests {
 
